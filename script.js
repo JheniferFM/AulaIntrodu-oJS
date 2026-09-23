381dadc7e90e829a6a0ed8973267f806dcabaf19
Jhenifer  Meneses <166198002+JheniferFM@users.noreply.github.com>
2026-09-22 21:52:34 -0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-let mensagem = document.getElementById("mensagem");
-let botao = document.getElementById("botao");
-
-botao.addEventListener("click", function() {
-    mensagem.textContent = "Você clicou no botão! 🎉";
-});
